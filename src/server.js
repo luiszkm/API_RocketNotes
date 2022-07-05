4257e16d1c35791459810a92f2dd6a264b109603
Luis Murilo <luiszkm@gmail.com>
2022-07-04 21:10:36 -0300

    add pm2

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 require("express-async-errors")
+require("dotenv/config")
 
 const AppError = require("./utils/appError")
 const migrationsRun = require("./database/sqlite/migrations")
@@ -35,7 +36,7 @@ app.use((error, request, response, next) => {
   })
 })
 
-const PORT = 3333
+const PORT = process.env.SERVER_PORT || 3333
 
 app.listen(PORT, () => {
   console.log(`to na porta ${PORT}`);
